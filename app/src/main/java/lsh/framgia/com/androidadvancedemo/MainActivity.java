8a3c38ff56a7f623f4c2af0ce9d03eeaa925e40d
Hai Luong <luongsonhai96@gmail.com>
2018-10-08 17:48:15 +0700

    Reactive X demo

--- app/src/main/java/lsh/framgia/com/androidadvancedemo/MainActivity.java
+++ app/src/main/java/lsh/framgia/com/androidadvancedemo/MainActivity.java
@@ -3,7 +3,12 @@ package lsh.framgia.com.androidadvancedemo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 import lsh.framgia.com.androidadvancedemo.githubdemo.search.GithubFragment;
 import lsh.framgia.com.androidadvancedemo.githubdemo.search.GithubPresenter;
 import lsh.framgia.com.androidadvancedemo.githubdemo.repository.UserRepository;
@@ -12,16 +17,23 @@ import lsh.framgia.com.androidadvancedemo.githubdemo.source.remote.UserRemoteDat
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GithubFragment githubFragment = new GithubFragment();
-        GithubPresenter githubPresenter = new GithubPresenter(
-                UserRepository.getInstance(UserRemoteDataSource.getInstance(),
-                        UserLocalDataSource.getInstance()));
-        githubPresenter.setView(githubFragment);
-        replaceFragment(githubFragment, false);
+//        GithubFragment githubFragment = new GithubFragment();
+//        GithubPresenter githubPresenter = new GithubPresenter(
+//                UserRepository.getInstance(UserRemoteDataSource.getInstance(),
+//                        UserLocalDataSource.getInstance()));
+//        githubPresenter.setView(githubFragment);
+//        replaceFragment(githubFragment, false);
+    }
+
+    private void hello(final String... names) {
+        Observable.fromArray("a", "b", "c");
+        Observable.just("a");
     }
 
     private void replaceFragment(Fragment fragment, boolean addToBackStack) {
